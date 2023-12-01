77d5a82e3298f6d82047af338e3fece4dc056bcf
John Lopez <purplejohn13@gmail.com>
2023-12-01 14:02:57 -0800

    added prompts for title and description

--- index.js
+++ index.js
@@ -2,7 +2,23 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of your project?',
+        },
+        {
+            
+        }
+    ])
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
